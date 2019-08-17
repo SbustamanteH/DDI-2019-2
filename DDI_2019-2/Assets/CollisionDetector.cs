fdeb154ce0cb4d44ca526e83644f2b4fdb2fb872
sbustamanteH <sbustamante@uabc.edu.mx>
2019-08-16 19:36:01 -0700

    Introduccion a Unity

---
+++ DDI_2019-2/Assets/CollisionDetector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+
+    void Start()
+    {
+         Debug.Log("Inicio");
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Colision detectada con "+ collision.gameObject.name);
+    }
+}
