8f7a6e486a5403084f34fca70d4c5a71f52e8e0e
sbustamanteH <sbustamante@uabc.edu.mx>
2019-09-13 19:42:15 -0700

    Avance practica 4.

---
+++ DDI_2019-2/Assets/ScriptsPract4/Coin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Coin", menuName = "Inventario/Items/Coin")]
+public class Coin : Item
+{
+    public override void Use()
+    {
+        base.Use();
+        Debug.Log("Usando item " + name);
+    }
+}
